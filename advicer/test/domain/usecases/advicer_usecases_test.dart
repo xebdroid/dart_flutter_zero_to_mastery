52205f9e50d3409398326e6a1f707f72b5e2fcc1
Martin Wollny <martin.wollny@joinmedia.de>
2024-01-16 15:32:22 +0100

    Unit testing temporary state

---
+++ advicer/test/domain/usecases/advicer_usecases_test.dart
@@ -0,0 +1,37 @@
+import 'package:advicer/domain/entities/advice_entity.dart';
+import 'package:advicer/domain/repositories/advicer_repository.dart';
+import 'package:advicer/domain/usecases/advicer_usecases.dart';
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import 'advicer_usecases_test.mocks.dart';
+
+@GenerateMocks([AdvicerRepository])
+void main() {
+  late AdvicerUsecases advicerUsecases;
+  late MockAdvicerRepository mockAdvicerRepository;
+
+  setUp(() {
+    mockAdvicerRepository = MockAdvicerRepository();
+    advicerUsecases =
+        AdvicerUsecases(advicerRepository: MockAdvicerRepository());
+  });
+
+  group('getAdviceUsecase', () {
+    final t_Advice = AdviceEntity(advice: 'test', id: 1);
+
+    test('should return the same advice as repository', () async {
+      // arrange
+      when(mockAdvicerRepository.getAdviceFromApi())
+          .thenAnswer((_) async => Right(t_Advice));
+
+      // act
+      final result = await advicerUsecases.getAdviceUsecase();
+
+      // assert
+      expect(result, Right(t_Advice));
+    });
+  });
+}
